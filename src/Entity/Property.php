2627cdc6b30397933da31adae31856a328c37083
Agamemnhon <damien.garnier007@gmail.com>
2021-02-22 10:04:18 +0100

    feat: Validation Données

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PropertyRepository::class)
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -26,6 +29,7 @@ class Property
     private $id;
 
     /**
+     * @Assert\Length(min=5, max=255)
      * @ORM\Column(type="string", length=255)
      */
     private $title;
@@ -37,6 +41,7 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(min=10, max=400)
      */
     private $surface;
 
@@ -76,11 +81,13 @@ class Property
     private $address;
 
     /**
+     * @Assert\Regex("/^[]0-9]{5}$/")
      * @ORM\Column(type="string", length=255)
      */
     private $postal_code;
 
     /**
+     *
      * @ORM\Column(type="boolean", options={"default": false })
      */
     private $sold = false;
